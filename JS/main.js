14514fa55f254eb3779a332653618d005b674b96
saifkabeer <70325102+saifkabeer@users.noreply.github.com>
2021-08-04 10:22:35 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu");
+const navigation = document.querySelector(".navigation");
+menu.addEventListener("click", () => { 
+    menu.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
